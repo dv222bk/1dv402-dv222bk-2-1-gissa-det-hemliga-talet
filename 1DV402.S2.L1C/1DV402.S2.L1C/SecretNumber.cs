1febb93253cbbc7a546cefdc6751cabdb6a580ab
Daniel Vedin <dv222bk@student.lnu.se>
2014-09-16 12:27:03 +0200

    Created GussedNumber.cs. Added quite a bit of code to SecretNumber.cs

--- 1DV402.S2.L1C/1DV402.S2.L1C/SecretNumber.cs
+++ 1DV402.S2.L1C/1DV402.S2.L1C/SecretNumber.cs
@@ -6,53 +6,41 @@ using System.Threading.Tasks;
 
 namespace _1DV402.S2.L1C
 {
-    class SecretNumber
+    public class SecretNumber
     {
         private GuessedNumber[] _guessedNumbers;
         private int? _number;
         public const int MaxNumberOfGuesses = 7;
-
+        
         public bool CanMakeGuess
         {
             public get
             {
-
-            }
-            private set
-            {
-
+                if(Count >= MaxNumberOfGuesses || Outcome == Outcome.Right)
+                {
+                    return false;
+                }
+                return true;
             }
         }
 
         public int Count
         {
-            public get
-            {
-
-            }
-            private set
-            {
-
-            }
+            public get;
+            private set;
         }
 
         public int? Guess
         {
-            public get
-            {
-
-            }
-            private set
-            {
-
-            }
+            public get;
+            private set;
         }
 
         public GuessedNumber[] GuessedNumbers 
         {
             public get
             {
-
+                return (GuessedNumber[])_guessedNumbers.Clone();
             }
         }
 
@@ -69,41 +57,69 @@ namespace _1DV402.S2.L1C
                     return _number;
                 }
             }
-            private set
-            {
-                _number = value;
-            }
+            private set { _number = value; }
         }
 
         public Outcome Outcome
         {
-            public get
-            {
-
-            }
-            private set
-            {
-
-            }
+            public get;
+            private set;
         }
 
-        public static SecretNumber()
-        {
-            Initialize();
-        }
-
-        public static void Initialize()
+        public void Initialize()
         {
             _guessedNumbers = new GuessedNumber[MaxNumberOfGuesses];
-            //Number = RandomInt
+            for (int i = 0; i < _guessedNumbers.Length; i++)
+            {
+                _guessedNumbers[i].Outcome = Outcome.Indefinite;
+            }
+            Random random = new Random();
+            Number = random.Next(1, 100);
             Count = 0;
             Guess = null;
             Outcome = Outcome.Indefinite;
         }
 
-        public static Outcome MakeGuess(int guess)
+        public Outcome MakeGuess(int guess)
         {
-
+            if (CanMakeGuess)
+            {
+                if (guess < 1 || guess > 100)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                Guess = guess;
+                Count++;
+                if (Array.IndexOf(GuessedNumbers, guess) > -1)
+                {
+                    Outcome = Outcome.OldGuess;
+                }
+                else if (Guess > _number)
+                {
+                    Outcome = Outcome.High;
+                }
+                else if (Guess < _number)
+                {
+                    Outcome = Outcome.Low;
+                }
+                else if (Guess == _number)
+                {
+                    Outcome = Outcome.Right;
+                }
+                else
+                {
+                    throw new ArgumentException("Ditt värde är jätte konstigt."); // This should never happen
+                }
+            }
+            else
+            {
+                Outcome = Outcome.NoMoreGuesses;
+            }
+            return Outcome;
+        }
+        public SecretNumber()
+        {
+            Initialize();
         }
     }
 }
